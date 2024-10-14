620b8a7237df612d188776fc5c9ee868cd654388
TheSakurist <131897311+TheSakurist@users.noreply.github.com>
2024-10-15 01:35:56 +0200

    Add files via upload

---
+++ CustomMessageBox.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
+public class CustomMessageBox : Window
+{
+    public CustomMessageBox(string message, Point location)
+    {
+        Title = "Message";
+        Content = new TextBlock
+        {
+            Text = message,
+            Margin = new Thickness(10)
+        };
+        Width = 300;
+        Height = 150;
+        WindowStartupLocation = WindowStartupLocation.Manual;
+        Left = location.X;
+        Top = location.Y;
+    }
+}
